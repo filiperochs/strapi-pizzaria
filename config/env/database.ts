f940f67b6c33def3fc89256d4f20b6d49c04363d
filiperochs <filipesilva.tmsi@gmail.com>
2023-06-03 22:20:13 -0300

    feat: console log configs

--- config/env/database.ts
+++ config/env/database.ts
@@ -1,24 +1,30 @@
 import { parse } from "pg-connection-string";
 const config = parse(process.env.DATABASE_URL);
 
-export default ({ env }) => ({
-  connection: {
-    client: "postgres",
+export default ({ env }) => {
+  const configuration = {
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: env.bool("DATABASE_SSL", false) && {
-        key: env("DATABASE_SSL_KEY", undefined),
-        cert: env("DATABASE_SSL_CERT", undefined),
-        ca: env("DATABASE_SSL_CA", undefined),
-        capath: env("DATABASE_SSL_CAPATH", undefined),
-        cipher: env("DATABASE_SSL_CIPHER", undefined),
-        rejectUnauthorized: false,
+      client: "postgres",
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: env.bool("DATABASE_SSL", false) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: false,
+        },
       },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+
+  console.log(configuration);
+
+  return configuration;
+};
