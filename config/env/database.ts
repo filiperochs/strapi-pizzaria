5a3f4e7a1f840c0d715e21f282ca2d7609ba58dc
filiperochs <filipesilva.tmsi@gmail.com>
2023-06-03 22:12:22 -0300

    fix: ssl

--- config/env/database.ts
+++ config/env/database.ts
@@ -10,7 +10,12 @@ export default ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: {
+      ssl: env.bool("DATABASE_SSL", false) && {
+        key: env("DATABASE_SSL_KEY", undefined),
+        cert: env("DATABASE_SSL_CERT", undefined),
+        ca: env("DATABASE_SSL_CA", undefined),
+        capath: env("DATABASE_SSL_CAPATH", undefined),
+        cipher: env("DATABASE_SSL_CIPHER", undefined),
         rejectUnauthorized: false,
       },
     },
