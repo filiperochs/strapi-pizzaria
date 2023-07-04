6c69e2f72a4a93872977cc026997f0872325ec1d
filiperochs <filipesilva.tmsi@gmail.com>
2023-07-04 01:11:35 -0300

    feat: path resolve

--- src/api/swaggerjson/controllers/swaggerjson.ts
+++ src/api/swaggerjson/controllers/swaggerjson.ts
@@ -1,12 +1,21 @@
 import fs from "fs";
+import path from "path";
 
 export default {
   async index(ctx) {
     ctx.send(
       fs.readFileSync(
-        "src\\extensions\\documentation\\documentation\\1.0.0\\full_documentation.json",
+        path.resolve(
+          __dirname,
+          "..\\..\\..\\extensions\\documentation\\documentation\\1.0.0\\full_documentation.json"
+        ),
         "utf8"
       )
     );
   },
 };
+
+// fs.readFileSync(
+//   "..\\..\\..\\extensions\\documentation\\documentation\\1.0.0\\full_documentation.json",
+//   "utf8"
+// )
