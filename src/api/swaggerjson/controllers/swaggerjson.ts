3f31a3558c1c817602d16ada6bf9c54ed8233a7a
filiperochs <filipesilva.tmsi@gmail.com>
2023-07-06 19:07:06 -0300

    fix: remove from prod env

--- src/api/swaggerjson/controllers/swaggerjson.ts
+++ src/api/swaggerjson/controllers/swaggerjson.ts
@@ -3,9 +3,13 @@ import path from "path";
 
 export default {
   async index(ctx) {
+    if (process.env.NODE_ENV === "production") return null;
     ctx.send(
       fs.readFileSync(
-        "/app/dist/src/extensions/documentation/documentation/1.0.0/full_documentation.json",
+        path.resolve(
+          __dirname,
+          "..\\..\\..\\extensions\\documentation\\documentation\\1.0.0\\full_documentation.json"
+        ),
         "utf8"
       )
     );
